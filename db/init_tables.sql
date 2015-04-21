edd17eddd9453cdaead0df56d30a7aec102e539c
LiShunyang <lishunyang@pku.edu.cn>
2015-04-21 15:34:16 +0800

    Add a ruby script to extract the empty features

--- db/init_tables.sql
+++ db/init_tables.sql
@@ -1,5 +1,6 @@
 -- 创建俩表，因为表明都指定好了，所以其他的shell脚本里的名字都不要改
 
+/*
 DROP TABLE train_item;
 CREATE TABLE train_item(
   item_id int8,
@@ -16,6 +17,7 @@ CREATE TABLE train_user(
   item_category integer,
   hours integer
 );
+*/
 
 /*
 -- DROP TABLE train_user_limit;
@@ -28,3 +30,26 @@ CREATE TABLE train_user_limit(
   hours integer
 );
 */
+
+/*
+DROP TABLE user_type;
+CREATE TABLE user_type(
+  user_id int8,
+  behavior_type integer,
+  max_itv int8,
+  min_itv int8,
+  average_itv int8,
+  variance_itv int8
+);
+*/
+
+DROP TABLE user_foremost_itv;
+CREATE TABLE user_foremost_itv(
+  user_id int8,
+  behavior_type integer,
+  max_itv int8,
+  min_itv int8,
+  average_itv int8,
+  variance_itv int8
+);
+
